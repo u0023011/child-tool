fd50058c34d4bf10bbf42e54512e017825fb5293
u0023011 <u0023011@gmail.com>
2026-07-09 14:02:27 +0800

    微調

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,13 @@
-self.addEventListener('fetch',e=>{});
+const CACHE_NAME = "child-tool-home-v14";
+const FILES_TO_CACHE = ["/","/index.html","/manifest.json","/icon-192.png","/icon-512.png"];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
