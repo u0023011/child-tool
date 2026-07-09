12f0249e30e40e382e607641b7be02096243407c
u0023011 <u0023011@gmail.com>
2026-07-09 11:37:29 +0800

    新圖檔

--- report-date-tool/service-worker.js
+++ report-date-tool/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "date-calc-tool-v5";
+const CACHE_NAME = "date-calc-tool-v6";
 const FILES_TO_CACHE = ["/","/index.html","/manifest.json","/icon-192.png","/icon-512.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
